b8fcbf18ebeb46bc29930f973a3eb86279247e4a
XaQ1997 <ksawery247@wp.pl>
2023-09-05 15:22:09 +0200

    Dodano klasę testową MusicalKeyTest

---
+++ app/src/test/java/com/example/musicpdf/MusicalKeyTest.kt
@@ -0,0 +1,44 @@
+package com.example.musicpdf
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MusicalKeyTest {
+    var testObj=MusicalKey(32u.toUByte(), "Testowy klucz", 'G', 0, "~/G_key.svg")
+
+    @Test
+    fun idTest()
+    {
+        assertEquals(32u.toUByte(), testObj.id)
+    }
+
+    @Test
+    fun charNameTest()
+    {
+        assertEquals("Klucz", testObj.charName)
+    }
+
+    @Test
+    fun keyNameTest()
+    {
+        assertEquals("Testowy klucz", testObj.keyName)
+    }
+
+    @Test
+    fun typeTest()
+    {
+        assertEquals('G', testObj.type)
+    }
+
+    @Test
+    fun heightTest()
+    {
+        assertEquals(0.toByte(), testObj.height)
+    }
+
+    @Test
+    fun imageTest()
+    {
+        assertEquals("~/G_key.svg", testObj.image)
+    }
+}
