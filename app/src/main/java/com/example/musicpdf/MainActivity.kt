9a5ffdb0ff92ad743699177e1370be5a0b061f1e
XaQ1997 <ksawery247@wp.pl>
2023-10-09 12:21:01 +0200

    Dodano pierwsze akcje przycisków

--- app/src/main/java/com/example/musicpdf/MainActivity.kt
+++ app/src/main/java/com/example/musicpdf/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.musicpdf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var newDocumentButton: Button = findViewById(R.id.new_document_button)
+        var loadDocumentButton: Button = findViewById(R.id.load_document_button)
+        var closeButton: Button = findViewById(R.id.close_button)
+
+        newDocumentButton.setOnClickListener{
+            setContentView(R.layout.activity_new_document)
+        }
+
+        closeButton.setOnClickListener{
+            finish()
+        }
     }
 }
