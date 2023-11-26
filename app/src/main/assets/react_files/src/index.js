0a1baa964ecd2915e908e4a942446aea6c15c5e3
XaQ1997 <ksawery247@wp.pl>
2023-11-26 12:27:14 +0100

    Aktualizacja kodu

--- app/src/main/assets/react_files/src/index.js
+++ app/src/main/assets/react_files/src/index.js
@@ -1,74 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const Header = function() {
-    return (
-      <h1 id="my-heading">
-        <span>Baza danych plików muzycznych w formacie <b>PDF</b></span>
-      </h1>
-    );
+  return (
+    <h1 id="my-heading">
+      <span>Baza danych plików muzycznych w formacie <b>PDF</b></span>
+    </h1>
+  );
 };
 
-function Table({ headers, initialData }) {
-    return (
-      <table>
-        <thead>
-          <tr>
-            {headers.map((title, idx) => (
-              <th key={idx}>{title}</th>
+function Table({ headers, data }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((title, idx) => (
+            <th key={idx}>{title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIdx) => (
+          <tr key={rowIdx}>
+            {row.map((cell, cellIdx) => (
+              <td key={cellIdx}>{cell}</td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {initialData.map((row, idx) => (
-            <tr key={idx}>
-              {row.map((cell, idx) => (
-                <td key={idx}>{cell}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+        ))}
+      </tbody>
+    </table>
+  );
 }
-
-Table.propTypes = {
-    headers: PropTypes.arrayOf(PropTypes.string),
-    initialData: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
-};
 
 const headers = ['Tytuł', 'Instrument'];
 
-// Komponent, który pobiera dane z API
 const FileTable = () => {
-    const [files, setFiles] = React.useState([]);
+  const [filesAndDirectories, setFilesAndDirectories] = useState([]);
 
-    React.useEffect(() => {
-      // Pobierz informacje o plikach z API
-      axios.get('/api/files')
-        .then(response => setFiles(response.data))
-        .catch(error => console.error('Błąd pobierania danych:', error));
-    }, []); // Pusta tablica oznacza, że useEffect wykona się tylko raz po zamontowaniu komponentu
+  useEffect(() => {
+    // Pobierz dane po zamontowaniu komponentu
+    fetchData();
 
-    return (
-      <Table headers={headers} initialData={files.map(file => [file.directory, <a href={file.link} target="_blank" rel="noopener noreferrer">{file.name}</a>])} />
-    );
+    // Utwórz EventSource, aby odbierać dane SSE
+    const eventSource = new EventSource('/sse');
+
+    // Obsługa przychodzących danych SSE
+    eventSource.onmessage = event => {
+      const updatedFilesAndDirectories = JSON.parse(event.data);
+      setFilesAndDirectories(updatedFilesAndDirectories);
+    };
+
+    // Oczyść EventSource po odmontowaniu komponentu
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  const fetchData = () => {
+    axios
+      .get('/api/files')
+      .then(response => setFilesAndDirectories(response.data))
+      .catch(error => console.error('Błąd pobierania danych:', error));
+  };
+
+  return (
+    <div>
+      {filesAndDirectories.map(directory => (
+        <div key={directory.directory}>
+          <h2>{directory.directory}</h2>
+          <Table
+            headers={headers}
+            data={directory.pdfFiles.map(file => [
+              <a
+                href={file.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {file.name}
+              </a>,
+            ])}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
-ReactDOM.render(
-    <div>
-      <Header />
-      <FileTable />
-    </div>,
-    document.getElementById('app')
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <div>
+    <Header />
+    <FileTable />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- app/src/main/assets/react_files/src/server.js
+++ app/src/main/assets/react_files/src/server.js
@@ -1,47 +1,82 @@
 const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const port = 8000;
-
 // Endpoint do pobierania listy plików
 app.get('/api/files', (req, res) => {
-  const directoryPath = path.join(__dirname, 'pdf_directory');
+  const files = getFilesAndDirectories(); // Funkcja do pobrania listy plików
+  res.json(files);
+});
 
-  // Odczytaj pliki z katalogu
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error('Błąd odczytu katalogu:', err);
-      res.status(500).json({ error: 'Błąd odczytu katalogu' });
-      return;
-    }
+// Endpoint SSE do przesyłania aktualizacji do przeglądarki
+app.get('/sse', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
 
-    // Przygotuj dane do wysłania
-    const fileData = files.map((fileName) => ({
-      name: fileName,
-      directory: directoryPath,
-      link: `/api/files/download/${fileName}`, // Endpoint do pobierania pliku
-    }));
+  const intervalId = setInterval(() => {
+    const files = getFilesAndDirectories(); // Pobierz najnowszą listę plików
+    res.write(`data: ${JSON.stringify(files)}\n\n`); // Wysyłaj dane do przeglądarki
+  }, 5000); // Interwał aktualizacji (zmień według potrzeb)
 
-    res.json(fileData);
+  // Zakończ interwał po zamknięciu połączenia z klientem
+  req.on('close', () => {
+    clearInterval(intervalId);
   });
 });
 
-// Endpoint do pobierania plików
-app.get('/api/files/download/:fileName', (req, res) => {
-  const fileName = req.params.fileName;
-  const filePath = path.join(__dirname, 'pdf_directory', fileName);
+const fetchData = () => {
+  axios
+    .get('http://localhost:3000/api/files')  // Zaktualizuj adres API
+    .then(response => setFilesAndDirectories(response.data))
+    .catch(error => console.error('Błąd pobierania danych:', error));
+};
 
-  // Wysyłanie pliku w odpowiedzi
-  res.download(filePath, fileName, (err) => {
-    if (err) {
-      console.error('Błąd podczas pobierania pliku:', err);
-      res.status(500).json({ error: 'Błąd pobierania pliku' });
-    }
+const eventSource = new EventSource('http://localhost:3000/sse');
+
+function getFilesAndDirectories() {
+  const directories = fs.readdirSync('./pdf-directory');
+  const filesAndDirectories = directories.map(directory => {
+    const files = fs.readdirSync(`./pdf-directory/${directory}`);
+    const pdfFiles = files
+      .filter(file => file.endsWith('.pdf'))
+      .map(file => ({
+        name: file,
+        link: `/pdf-directory/${directory}/${file}`,
+      }));
+
+    return { directory, pdfFiles };
+  });
+
+  return filesAndDirectories;
+}
+
+// Dodaj poniższy kod na końcu pliku
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.get('/sse', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  const intervalId = setInterval(() => {
+    const filesAndDirectories = getFilesAndDirectories(); // Pobierz najnowszą listę plików i katalogów
+    res.write(`data: ${JSON.stringify(filesAndDirectories)}\n\n`); // Wysyłaj dane do przeglądarki
+  }, 5000); // Interwał aktualizacji (zmień według potrzeb)
+
+  // Zakończ interwał po zamknięciu połączenia z klientem
+  req.on('close', () => {
+    clearInterval(intervalId);
   });
 });
 
-app.listen(port, () => {
-  console.log(`Serwer nasłuchuje na porcie ${port}`);
+http.listen(8000, () => {
+  console.log('Serwer nasłuchuje na porcie 8000');
 });
