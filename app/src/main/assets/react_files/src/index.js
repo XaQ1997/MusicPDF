45cec70e5ccbaedcc9b2982eed1a19f59bbea0c0
XaQ1997 <ksawery247@wp.pl>
2024-01-07 16:49:30 +0100

    Działające przejście do podstron trybów

--- app/src/main/assets/react_files/src/index.js
+++ app/src/main/assets/react_files/src/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const Home = () => (
   <div>
     <h2>Strona Główna</h2>
-    <nav>
-      <ul>
-        <li><button><Link to="/master">Przejdź do Master</Link></button></li>
-        <li><button><Link to="/slave">Przejdź do Slave</Link></button></li>
-      </ul>
-    </nav>
+        <button><a href="master.html">Dyrygent</a></button>
+        <br>
+        <button><a href="slave.html">Muzyk</a></button>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router>
-    <Home />
-  </Router>
-);
+root.render(<Home />);
