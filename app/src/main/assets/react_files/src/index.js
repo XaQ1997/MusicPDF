0dbcddc2c5835489e1b8bcc98220634d812f147c
XaQ1997 <ksawery247@wp.pl>
2024-01-07 17:16:44 +0100

    Aktualizacja przysicsku na stronie głównej

--- app/src/main/assets/react_files/src/index.js
+++ app/src/main/assets/react_files/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Home = () => (
-  <div>
+  <>
     <h2>Strona Główna</h2>
-        <button><a href="master.html">Dyrygent</a></button>
-        <br>
-        <button><a href="slave.html">Muzyk</a></button>
-  </div>
+    <button onClick={() => window.location.href = 'master.html'}>Dyrygent</button>
+    <br />
+    <button onClick={() => window.location.href = 'slave.html'}>Muzyk</button>
+  </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
